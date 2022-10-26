c2f8bc2bdc9ba4315d2f7dc7e7773f003e199791
@nimmet <nimetullah@live.com>
2022-10-26 20:49:45 +0300

    Completed all the codes and it works as expected

--- React_Projects/tours/src/Menu/App.js
+++ React_Projects/tours/src/Menu/App.js
@@ -1,28 +1,87 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 
 function App() {
-  const data = items;
+
+  const [data,setData] = useState(items)
+ 
   const categoryNames = new Set();
-  data.map((item) => categoryNames.add(item.category));
+  items.map((item) => categoryNames.add(item.category));
   const navList = [...categoryNames];
+
+  const clickAll = ()=> setData(items)
+  
+
+  const clickBreakfast = ()=>{
+    setData(()=> {
+        return (
+            items.filter((item)=> item.category === 'breakfast')
+        )
+    })
+  }
+
+  const clickLunch = ()=>{
+    setData(()=> {
+        return (
+            items.filter((item)=> item.category === 'lunch')
+        )
+    })
+  }
+
+  const clickShakes = ()=>{
+    setData(()=> {
+        return (
+            items.filter((item)=> item.category === 'shakes')
+        )
+    })
+  }
+ 
+ 
 
   return (
     <div>
       <div className=" text-center my-16">
-      <h1 className=" capitalize font-bold text-4xl ">our menu</h1>
-      <div className=" w-20 h-[3px] bg-yellow-600 mx-auto my-2"></div>
+        <h1 className=" capitalize font-bold text-4xl ">our menu</h1>
+        <div className=" w-20 h-[3px] bg-yellow-600 mx-auto my-2"></div>
       </div>
-      <nav className=" capitalize text-yellow-600">
-        <ul className="flex flex-row mx-64 sm:mx-32 justify-center">
-        <li className=" flex-1">All</li>
-          {navList.map((nam) => (
-            <li className=" flex-1">{nam}</li>
+      <nav className=" capitalize text-yellow-600 py-4">
+        <ul className="flex flex-row mx-10 px-1 text-center justify-center font-bold">
+          <li className=" flex-initial mx-8 hover:border hover:rounded-md hover:bg-yellow-400 hover:text-white px-2 py-[1px] " onClick={clickAll}>All
+          </li>
+          {navList.map((cat, index) => (
+            <li
+              key={index}
+              name={cat}
+              className="  mx-8  hover:border hover:rounded-md hover:bg-yellow-400 hover:text-white  px-2 py-[1px] "
+              onClick={(e)=>{
+                
+                if(e.target.textContent ==="breakfast"){
+                    clickBreakfast()
+                }else if(e.target.textContent==='lunch'){
+                    clickLunch()
+                }else{
+                    clickShakes()
+                }
+              }
+              }
+            >
+              {cat}
+            </li>
           ))}
         </ul>
       </nav>
+
+      <div className=" flex flex-wrap sm:flex justify-between mx-5">
+        {
+            data.map((d)=> {
+               return <Menu {...d}/>
+            })
+
+    
+        }
+      </div>
     </div>
   );
 }
--- React_Projects/tours/src/Menu/Menu.js
+++ React_Projects/tours/src/Menu/Menu.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+
+const Menu = ({id,title,price,img,desc}) => {
+ 
+
+  return (
+    <div key={id} className="mx-5 my-5 columns-2 w-[47%]">
+    <img className=" w-60 h-40  border-yellow-600 border-2 rounded float-left my-6" src={img} alt={title} />
+
+      <div className=" -ml-16 float-right">
+      <div className="  border-b my-5 flex">
+      <h1 className="inline font-bold capitalize  flex-1">{title}</h1>
+      <h1 className="inline font-bold text-yellow-700 ">${Math.floor(price)}</h1>
+      </div>
+      <p className=" text-sky-800">{desc}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
+
+
