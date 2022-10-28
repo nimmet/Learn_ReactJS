006c08837cce16e30865b2a21b72ac37e361f0d2
@nimmet <nimetullah@live.com>
2022-10-28 19:17:58 +0300

    Project Completed

--- React_Projects/tours/src/Grocery-Bud/App.js
+++ React_Projects/tours/src/Grocery-Bud/App.js
@@ -1,8 +1,70 @@
 import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
+import { FaEdit, FaTrash } from 'react-icons/fa'
+
 
 function App() {
+
+  const [val,setVal] = useState('')
+  const [list,setList] = useState([])
+  const [editable,setEditable] = useState(false)
+  const [editableId,setEditableId] = useState(0)
+  const [editID,setEditID] = useState(0)
+
+
+ 
+
+  const handleClick= (e) => {
+    e.preventDefault()
+    let newVal = e.target.value
+    setVal(newVal)
+
+  }
+
+  const handleSubmit = (e)=> {
+    e.preventDefault()
+    const newList = {id:new Date().getTime().toString(), value:val}
+    setList([...list,newList])
+
+    if(editable){
+     const changed = list.filter((li)=>li.value===val)
+     const changedList = list.filter((li)=>li.id !==editableId)
+     console.log(changedList);
+     const newEditItem = {id:editableId,value:val}
+     setList([...changedList,newEditItem])
+     setVal("")
+     setEditable(false)
+     console.log(changed);
+    }
+    setVal("")
+  }
+
+
+ 
+
+  const delItem = (id)=>{
+    setList((list)=> {
+     return list.filter((l)=>  l.id !== id
+     )
+    })
+  }
+
+  const editItem = (id)=> {
+    
+    
+      const editableItem = list.filter((li)=> li.id === id)
+      setVal(editableItem[0].value)
+      setEditableId(id)
+     
+      setEditable(true)
+      // console.log(editedItem);
+      
+    
+  }
+
+ 
+
   return <>
     <div className=' bg-slate-200 w-4/6 mx-auto shadow-lg shadow-neutral-500 lg:w-2/5 rounded-md'>
 
@@ -10,11 +72,33 @@ function App() {
     <h1 className=' capitalize font-[700] text-2xl'>grocery bud</h1>
 </div>
 
-<div className='text-center'>
-<form action="" className='py-5'>
-    <input type="text" placeholder='e.g. eggss' className='rounded px-2 w-64 bg-sky-50'/>
-    <button className='px-4 rounded-md bg-sky-300 mx-2'>Submit</button>
+<div className='text-center py-2'>
+<form action="" className='py-5' onSubmit={handleSubmit}>
+    <input type="text" placeholder='e.g. eggs' className='rounded ml-2 px-2 w-64 bg-sky-50' value={val}
+      onChange={handleClick }
+    />
+    <button className='px-4 rounded-md bg-sky-300 mx-2' onClick={handleSubmit}>{editable ? 'Edit' : 'Submit'}</button>
 </form>
+</div>
+
+
+<div className=''>
+  
+    {
+     list.map((l)=>{
+      const {id,value} = l
+      return (
+      <div key={id} className='flex bg-green-100 mx-7 my-2 py-2 rounded-md shadow-md'>
+
+      <h1 className='flex-1 mx-2 font-[500] capitalize' >{value.toLowerCase()}</h1>
+        <FaEdit className=' text-red-700 my-auto' onClick={()=>editItem(id)}/>
+        <FaTrash className='mr-3 ml-2 text-red-700 my-auto'  onClick={()=>delItem(id)}/>
+        
+      </div>
+      )
+     })
+    }
+   
 </div>
 
 
