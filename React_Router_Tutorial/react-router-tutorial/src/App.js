be5df0bd893e520705a7f9d15b42ee9c5a7d05b9
@nimmet <nimetullah@live.com>
2022-10-30 12:02:57 +0300

    React Router tutorial

---
+++ React_Router_Tutorial/react-router-tutorial/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {BrowserRouter,Routers,Route} from 'react-router-dom'
+
+function App() {
+  return <BrowserRouter>
+      <Routers>
+        <Route path='/'
+      </Routers>
+    </BrowserRouter>
+    
+  
+}
+
+export default App;
