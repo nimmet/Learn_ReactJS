f66922afe0f3122e47437ed1b12a6528932e2c8e
@nimmet <nimetullah@live.com>
2022-11-02 11:34:37 +0300

    used Router to reconstruct hopepage

--- Master_Project/Crown-Clothing/crown-clothing/src/App.js
+++ Master_Project/Crown-Clothing/crown-clothing/src/App.js
@@ -1,10 +1,17 @@
 import './App.scss';
 import Homepage
  from './pages/homepage/homepage.component';
+
+import { Routes,Route } from 'react-router-dom'
+
+
+
 function App() {
   return (
    <div>
-    <Homepage/>
+  <Routes>
+    <Route path="/" element={<Homepage />}></Route>
+  </Routes>
    </div>
   );
 }
---
+++ Master_Project/Crown-Clothing/crown-clothing/src/routes/home/home.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>home.component</div>
+  )
+}
+
+export default Home
