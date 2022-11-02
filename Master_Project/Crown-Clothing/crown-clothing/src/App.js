065e0935a2b3fd707a423eca082d07f3c9ad134d
@nimmet <nimetullah@live.com>
2022-11-02 12:17:47 +0300

    implemeted <Outlet>

--- Master_Project/Crown-Clothing/crown-clothing/src/App.js
+++ Master_Project/Crown-Clothing/crown-clothing/src/App.js
@@ -2,17 +2,35 @@ import './App.scss';
 import Homepage
  from './pages/homepage/homepage.component';
 
-import { Routes,Route } from 'react-router-dom'
+import { Routes,Route,Outlet } from 'react-router-dom'
 
+const Shop = ()=> {
+  return (
+    <div className="">
+      <h1>I am the shop page</h1>
+        </div>
+  )
+}
 
+const Navigation = ()=> {
+  return (
+    <div className="">
+    <div>
+      <h1>I am the Navigation bar</h1>
+    </div>
+    <Outlet/>
+        </div>
+  )
+  }
 
 function App() {
   return (
-   <div>
   <Routes>
-    <Route path="/" element={<Homepage />}></Route>
+  <Route path="/" element={<Navigation/>} >
+    <Route index element={<Homepage />}/>
+    <Route path="shop" element={<Shop />}/>
+    </Route>
   </Routes>
-   </div>
   );
 }
 
--- Master_Project/Crown-Clothing/crown-clothing/src/pages/homepage/homepage.component.jsx
+++ Master_Project/Crown-Clothing/crown-clothing/src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './homepage.styles.scss'
 import Directory from '../../components/directory/directoy.component'
+import { Outlet } from 'react-router-dom'
 
 const HomePage = () => (
     <div className='homepage'>
         <Directory />
+    <Outlet/>
     </div>
   )
 
