0309176d0b22b3ed1e1272b59e98a394f6d2caa6
@nimmet <nimetullah@live.com>
2022-11-06 20:57:57 +0300

    shopping cart

--- Master_Project/Crown-Clothing/crown-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ Master_Project/Crown-Clothing/crown-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,22 @@
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/cart.context'
 import './cart-dropdown.styles.scss'
 import Button from '../button/button.component'
-import React from 'react'
+import CartItem from '../cart-item/cart-item.component'
+
 
 const CartDrowdown = () => {
+    const {cartItems} = useContext(CartContext)
   return (
     <div className='cart-dropdown-container'>
         <div className='cart-items'>
-            <Button>GO TO CHECKOUT</Button>
+        {
+            cartItems.map((item) => (
+                <CartItem key={item.id} cartItem={item}/>
+            ))
+        }
         </div>
+            <Button>GO TO CHECKOUT</Button>
     </div>
   )
 }
--- Master_Project/Crown-Clothing/crown-clothing/src/routes/navigation/navigation.component.jsx
+++ Master_Project/Crown-Clothing/crown-clothing/src/routes/navigation/navigation.component.jsx
@@ -4,6 +4,7 @@ import { Fragment , useContext} from "react"
 import {ReactComponent as CrownLogo} from '../../assets/crown.svg'
 import './navigation.styles.scss'
 import { UserContext } from "../../contexts/user.context"
+import { CartContext } from "../../contexts/cart.context"
 import {CartProvider} from "../../contexts/cart.context"
 import { signOutUser } from "../../utils/firebase/firebase.utils"
 import CartIcon from "../../components/cart-icon/cart-icon.component"
@@ -12,7 +13,7 @@ import CartDrowdown from "../../components/cart-dropdown/cart-dropdown.component
 
 const Navigation = ()=> {
   const {currentUser} = useContext(UserContext)
-  const {isCartOpen} = useContext(CartProvider)
+  const {isCartOpen} = useContext(CartContext)
 
     return (
       <Fragment>
