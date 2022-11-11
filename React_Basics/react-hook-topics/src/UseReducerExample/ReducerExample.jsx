3936c74dcb3e0a9a874e118fbefc8940b5af98f2
@nimmet <nimetullah@live.com>
2022-11-11 18:30:53 +0300

    added more element to reducer initialState

--- React_Basics/react-hook-topics/src/UseReducerExample/ReducerExample.jsx
+++ React_Basics/react-hook-topics/src/UseReducerExample/ReducerExample.jsx
@@ -3,29 +3,40 @@ import React from 'react'
 import { useReducer } from 'react'
 
 const ReducerExample = () => {
-    const initialState = {count:0}
+    const initialState = {count:0,name:'uyghur',age:20}
     const reducer= (state, action)=>{
         if(action.type==='increment'){
-            return {count:state.count+action.paylod}
+            return {...state,name:'Almas', count:state.count+1}
     }
 
     if(action.type ==='decrement'){
-        return {count:state.count-action.paylod}
+        return {name:'Ahmet',age:state.age-action.paylod,count:state.count-action.paylod}
 }
 
     if(action.type==='reset'){
-        return {count:0}
+        return {name:'Samet',age:0,count:0}
     }
 
 
     }
     
     const [state,dispatch] = useReducer(reducer, initialState)
+    const {name,age,count} = state 
     return (
-    <div> <h1>Reducer Example</h1>
-    <h1>Count: {state.count}</h1>
+    <div className='flex justify-center items-center mt-10'>
+
+        <div>
+
+    <div className=' divide-y-2'> <h1>Reducer Example</h1>
+    <h1>Name: {name}</h1>
+    <h1>Age: {age}</h1>
+    <h1 className=''>Count: {count}</h1>
+        </div>
+
+    <div className='flex gap-3'>
+
     <button onClick={()=>{
-        dispatch({type:'increment',paylod:1})
+        dispatch({type:'increment'})
     }}>increment</button>
 
 <button onClick={()=>{
@@ -35,7 +46,9 @@ const ReducerExample = () => {
 <button onClick={()=>{
         dispatch({type:'reset'})
     }}>reset</button>
+    </div>
 
+    </div>
     </div>
   )
 }
