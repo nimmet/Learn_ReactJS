f17c3ddaeb08fb49cc2dfd79ce5feea53407701e
@nimmet <nimetullah@live.com>
2022-11-15 17:40:39 +0300

    button disable functionality added

--- React_Basics/react-hook-topics/src/Shopping/components/Cart/index.jsx
+++ React_Basics/react-hook-topics/src/Shopping/components/Cart/index.jsx
@@ -28,12 +28,12 @@ const Cart = () => {
             <h1 className="mx-5 px-1 w-[98px]  ">{title.substring(0,10)}</h1>
             <div className=" flex-row flex justify-center items-center flex-1">
 
-            {quantity === 1 ? '':
+            {quantity === 1 ? <BiMinus size={30} className='' fill='gray'  />:
             <BiMinus size={30} className='cursor-pointer' fill={`${quantity===1 ? 'gray': 'red'}`}  onClick={()=>removeFromCart(item.id,'partial')} />
 
             }
             <h1 className="mx-4 " >{quantity}</h1>
-            <BiPlus size={30} className='cursor-pointer' fill="green" onClick={()=> addToCart(item)}/>
+            <BiPlus size={30} className='cursor-pointer' fill="green" onClick={()=> addToCart(item)} disabled={true}/>
             </div>
             <h1 className="mx-1 flex-1  font-bold">{price} $</h1>
             <button className=' bg-amber-500 rounded-2xl px-3 py-1 my-2 font-[400] outline outline-yellow-600 mr-4' onClick={()=>removeFromCart(item.id,'full')}>Remove</button>
